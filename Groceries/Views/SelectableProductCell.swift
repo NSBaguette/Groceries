9180f4a5d95e4e471ca724c58148d87e7fcfe21a
NSBaguette <nsbaguette@gmail.com>
2018-02-16 13:06:36 +0200

    Add draft cell

---
+++ Groceries/Views/SelectableProductCell.swift
@@ -0,0 +1,148 @@
+//
+//  SelectableProductCell.swift
+//  Groceries
+//
+//  Created by Illia Akhaiev on 2/9/18.
+//  Copyright © 2018 Illia Akhaiev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum SelectableProductCellState {
+    case selected
+    case notSelected
+}
+
+fileprivate final class SelectableProductCellInternals {
+    var checkmarkOffset = UIEdgeInsets.zero
+}
+
+fileprivate final class Ruler {
+    static func checkmarkOffset(from offset: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, offset + 1, 0, 5)
+    }
+}
+
+final class SelectableProductCell: UITableViewCell {
+    private var checkmark: UIImageView = UIImageView()
+    private var state: SelectableProductCellState = .notSelected
+    private(set) var cellTextLabel = UILabel()
+    private var internals = SelectableProductCellInternals()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        constructInterface()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        constructInterface()
+    }
+    
+    private func constructInterface() {
+        contentView.addSubview(cellTextLabel)
+        internals.checkmarkOffset = Ruler.checkmarkOffset(from: self.separatorInset.left)
+        accessoryType = .detailButton
+        
+        contentView.addSubview(checkmark)
+        checkmark.image = checkmarkImage(for: state)
+        applyConstraints(internals: internals)
+    }
+    
+    private func checkmarkImage(for state: SelectableProductCellState) -> UIImage {
+        switch state {
+        case .selected:
+            return UIImage(named: "checkmark-selected")!
+        case .notSelected:
+            return UIImage(named: "checkmark-empty")!
+        }
+    }
+}
+
+extension SelectableProductCell {
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+    }
+    
+    private func applyConstraints(internals: SelectableProductCellInternals) {
+        checkmark.translatesAutoresizingMaskIntoConstraints = false
+        cellTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        var constraint = NSLayoutConstraint(item: checkmark,
+                                            attribute: .centerY,
+                                            relatedBy: .equal,
+                                            toItem: contentView,
+                                            attribute: .centerY,
+                                            multiplier: 1,
+                                            constant: 0)
+        contentView.addConstraint(constraint)
+        
+        constraint = NSLayoutConstraint(item: checkmark,
+                                        attribute: .left,
+                                        relatedBy: .equal,
+                                        toItem: contentView,
+                                        attribute: .left,
+                                        multiplier: 1,
+                                        constant: internals.checkmarkOffset.left)
+        contentView.addConstraint(constraint)
+
+        constraint = NSLayoutConstraint(item: checkmark,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: contentView,
+                                        attribute: .height,
+                                        multiplier: 0.5,
+                                        constant: 0)
+        contentView.addConstraint(constraint)
+
+        constraint = NSLayoutConstraint(item: checkmark,
+                                        attribute: .width,
+                                        relatedBy: .equal,
+                                        toItem: checkmark,
+                                        attribute: .height,
+                                        multiplier: 1,
+                                        constant: 0)
+        contentView.addConstraint(constraint)
+        
+        //
+        constraint = NSLayoutConstraint(item: cellTextLabel,
+                                        attribute: .left,
+                                        relatedBy: .equal,
+                                        toItem: checkmark,
+                                        attribute: .right,
+                                        multiplier: 1,
+                                        constant: internals.checkmarkOffset.right)
+        contentView.addConstraint(constraint)
+        
+        constraint = NSLayoutConstraint(item: cellTextLabel,
+                                        attribute: .right,
+                                        relatedBy: .equal,
+                                        toItem: contentView,
+                                        attribute: .right,
+                                        multiplier: 1,
+                                        constant: 0)
+        contentView.addConstraint(constraint)
+        
+        constraint = NSLayoutConstraint(item: cellTextLabel,
+                                        attribute: .centerY,
+                                        relatedBy: .equal,
+                                        toItem: contentView,
+                                        attribute: .centerY,
+                                        multiplier: 1,
+                                        constant: 0)
+        contentView.addConstraint(constraint)
+        
+        constraint = NSLayoutConstraint(item: cellTextLabel,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: contentView,
+                                        attribute: .height,
+                                        multiplier: 1,
+                                        constant: 0)
+        contentView.addConstraint(constraint)
+    }
+}
