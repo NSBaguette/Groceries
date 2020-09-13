c910e7a064b7cfff1f83fc0cdf36739116594e67
Illia Akhaiev <26223450+grandboum@users.noreply.github.com>
2020-09-13 18:39:23 +0300

    Update to Xcode 11.7

--- Groceries/AppDelegate.swift
+++ Groceries/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
 
         let path = Librarian.databasePath()
